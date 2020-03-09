0ff1368efcacd87f6837c9227043faedd9bd6f5f
Ronak Malik <ronakmalik4242@gmail.com>
2020-03-09 14:41:57 -0500

    Remove cross compiler check on kernel.c

--- dentos/kernel/kernel.c
+++ dentos/kernel/kernel.c
@@ -3,13 +3,7 @@
 
 #include <kernel/tty.h>
 
-#if defined(__linux__)
-	#error "This code must be compiled with a cross-compiler"
-#elif !defined(__i386__)
-	#error "This code must be compiled with an x86-elf compiler"
-#endif
-
-// Main method called by start
+// Main method called by boot
 void kernel_main() {
 	term_init();
 
