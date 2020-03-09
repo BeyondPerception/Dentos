97d7fc481b12b34c6a1dd4e19c7bdb0d5c39884d
Ronak Malik <ronakmalik4242@gmail.com>
2020-03-09 18:48:08 -0500

    Add clean shortcut to qemu script and remove nographic option as it was causing problems

--- qemu.sh
+++ qemu.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -e
-. ./scripts/iso.sh
 
-qemu-system-"$(./scripts/target-triplet-to-arch.sh "$HOST")" \
-			-cdrom dentos.iso \
-			-nographic
+if [ "$1" = "clean" ]; then
+  ./scripts/clean.sh
+else
+  ./scripts/clean.sh
+  . ./scripts/iso.sh
+
+  qemu-system-"$(./scripts/target-triplet-to-arch.sh "$HOST")" \
+    -cdrom dentos.iso
+fi
