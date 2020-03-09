08a8200d011fcca5490a76cb8cf4ce82cffc32c9
Ronak Malik <ronakmalik4242@gmail.com>
2020-03-09 13:58:07 -0500

    Add clean option to build script

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 set -e
-. ./headers.sh
+if [[ "$1" = "clean" ]]; then
+	echo "Cleaning projects"
+	. ./config.sh
 
-for PROJECT in $PROJECTS; do
-	(cd $PROJECT && DESTDIR="$SYSROOT" $MAKE install)
-done
+	for PROJECT in $PROJECTS; do
+	  (cd $PROJECT && $MAKE clean)
+	done
+
+	rm -rf sysroot
+	rm -rf isodir
+	rm -rf myos.iso
+else
+	echo "Building projects"
+	. ./headers.sh
+
+	for PROJECT in $PROJECTS; do
+    	(cd $PROJECT && DESTDIR="$SYSROOT" $MAKE install)
+	done
+fi
