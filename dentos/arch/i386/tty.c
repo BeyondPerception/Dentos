ce46f1c68c2e869b6591a6effe8e9a036cfef066
Ronak Malik <ronakmalik4242@gmail.com>
2020-03-11 22:29:36 -0500

    Fix scrolling to use size of vga buffer pointer and not size of char which was causing problems in line wrapping

--- dentos/arch/i386/tty.c
+++ dentos/arch/i386/tty.c
@@ -1,9 +1,6 @@
-#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
-
-#include <kernel/tty.h>
 
 #include "vga.h"
 
@@ -17,56 +14,61 @@ static uint8_t term_color;
 static uint16_t* term_buffer;
 
 void term_putcharat(unsigned char c, uint8_t color, size_t row, size_t col) {
-    const size_t index = row * VGA_WIDTH + col;
-    term_buffer[index] = vga_entry(c, color);
+	const size_t index = row * VGA_WIDTH + col;
+	term_buffer[index] = vga_entry(c, color);
 }
 
 void term_init(void) {
-    term_row = 0;
-    term_col = 0;
-    term_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-    term_buffer = VGA_MEMORY;
-    for (size_t r = 0; r < VGA_HEIGHT; r++) {
-        for (size_t c = 0; c < VGA_WIDTH; c++) {
-            term_putcharat(' ', term_color, r, c);
-        }
-    }
+	term_row = 0;
+	term_col = 0;
+	term_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	term_buffer = VGA_MEMORY;
+	for (size_t r = 0; r < VGA_HEIGHT; r++) {
+		for (size_t c = 0; c < VGA_WIDTH; c++) {
+			term_putcharat(' ', term_color, r, c);
+		}
+	}
 }
 
 void term_setcolor(uint8_t color) {
-    term_color = color;
+	term_color = color;
 }
 
 void scroll(int n) {
-    for (int i = 0; i < n; i++) {
-        for (size_t j = 0; j < VGA_HEIGHT - 1; j++) {
-            memcpy(&term_buffer[j * VGA_WIDTH], &term_buffer[(j + 1) * VGA_WIDTH], VGA_WIDTH * sizeof(int));
-        }
-        memset(&term_buffer[(VGA_WIDTH - 1) * VGA_HEIGHT - 1], 0, VGA_WIDTH * sizeof(int));
-    }
+	for (int i = 0; i < n; i++) {
+		for (size_t j = 0; j < VGA_HEIGHT - 1; j++) {
+			memcpy(&term_buffer[j * VGA_WIDTH], &term_buffer[(j + 1) * VGA_WIDTH], VGA_WIDTH * sizeof(uint16_t));
+		}
+		for (size_t j = 0; j < VGA_WIDTH; j++) {
+			term_putcharat(' ', term_color, VGA_HEIGHT - 1, j);
+		}
+	}
 }
 
 void term_putc(char c) {
-    unsigned char uc = c;
-    if (uc == '\n') {
-        term_row++;
-        term_col = 0;
-    } else {
-        term_putcharat(uc, term_color, term_row, term_col);
-    }
-    if (++term_col == VGA_WIDTH) {
-        term_col = 0;
-        if (++term_row == VGA_HEIGHT)
-            scroll(1);
-    }
+	if (term_row == VGA_HEIGHT) {
+		scroll(1);
+		term_row--;
+		term_col = 0;
+	}
+	if (c == '\n') {
+		term_row++;
+		term_col = 0;
+	} else {
+		term_putcharat(c, term_color, term_row, term_col);
+		if (++term_col == VGA_WIDTH) {
+			term_col = 0;
+			term_row++;
+		}
+	}
 }
 
 void term_write(const char* data, size_t size) {
-    for (size_t i = 0; i < size; i++) {
-        term_putc(data[i]);
-    }
+	for (size_t i = 0; i < size; i++) {
+		term_putc(data[i]);
+	}
 }
 
 void term_writestring(const char* data) {
-    term_write(data, strlen(data));
+	term_write(data, strlen(data));
 }
