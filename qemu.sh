b974d367b71fd57e887ae0b2069a0b3874f17905
Ronak Malik <ronakmalik4242@gmail.com>
2020-03-11 22:27:19 -0500

    Change clean function in qemu script to mean clean up when finished

--- qemu.sh
+++ qemu.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 set -e
+./scripts/clean.sh
+. ./scripts/iso.sh
+
+qemu-system-"$(./scripts/target-triplet-to-arch.sh "$HOST")" \
+  -cdrom dentos.iso
 
 if [ "$1" = "clean" ]; then
   ./scripts/clean.sh
-else
-  ./scripts/clean.sh
-  . ./scripts/iso.sh
-
-  qemu-system-"$(./scripts/target-triplet-to-arch.sh "$HOST")" \
-    -cdrom dentos.iso
 fi
