08be9a4dbdccf0900d588cb4f7d58dc9a45e9e3f
Ronak Malik <ronakmalik4242@gmail.com>
2020-03-12 21:09:14 -0500

    Abstract clearing the terminal into its own function and check if a character is printable before printing

--- dentos/arch/i386/tty.c
+++ dentos/arch/i386/tty.c
@@ -1,7 +1,6 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
-#include <stdio.h>
 
 #include "vga.h"
 
@@ -19,12 +18,16 @@ void term_putcharat(unsigned char c, uint8_t color, size_t row, size_t col) {
 	term_buffer[index] = vga_entry(c, color);
 }
 
-void term_init(void) {
+void term_clear() {
+	memset(term_buffer, 0, VGA_HEIGHT * VGA_WIDTH * sizeof(uint16_t));
 	term_row = 0;
 	term_col = 0;
+}
+
+void term_init(void) {
 	term_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
 	term_buffer = VGA_MEMORY;
-	memset(term_buffer, 0, VGA_HEIGHT * VGA_WIDTH * sizeof(uint16_t));
+	term_clear();
 }
 
 void term_setcolor(uint8_t color) {
@@ -33,7 +36,7 @@ void term_setcolor(uint8_t color) {
 
 void scroll(int n) {
 	if (n >= VGA_HEIGHT) {
-		term_init();
+		term_clear();
 	} else {
 		memcpy(&term_buffer[0], &term_buffer[n * VGA_WIDTH], VGA_WIDTH * sizeof(uint16_t) * (VGA_HEIGHT - n));
 		memset(&term_buffer[(VGA_HEIGHT - 1) * VGA_WIDTH - 1], 0,
@@ -50,7 +53,7 @@ void term_putc(char c) {
 	if (c == '\n') {
 		term_row++;
 		term_col = 0;
-	} else {
+	} else if (c >= ' ') {
 		term_putcharat(c, term_color, term_row, term_col);
 		if (++term_col == VGA_WIDTH) {
 			term_col = 0;
