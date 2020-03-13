ef5ad343f6f1a94ef1da3a1d16c39ec153dba826
Ronak Malik <ronakmalik4242@gmail.com>
2020-03-13 14:46:21 -0500

    Setup and configure serial port during boot sequence

--- dentos/kernel/kernel.c
+++ dentos/kernel/kernel.c
@@ -1,11 +1,17 @@
-#include <stddef.h>
-#include <stdint.h>
 #include <stdio.h>
 
+#include <kernel/serial.h>
 #include <kernel/tty.h>
 
 // Main method called by boot
 void kernel_main() {
+	serial_configure_baud_rate(SERIAL_COM1_BASE, 3);
+	serial_configure_line(SERIAL_COM1_BASE);
+	serial_configure_buffer(SERIAL_COM1_BASE);
+	serial_configure_modem(SERIAL_COM1_BASE);
+
+	printk("%s\n", "Serial setup successfully");
+
 	term_init();
 
 	puts("Welcome to the terminal!");
