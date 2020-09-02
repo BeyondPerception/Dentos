e58f10526c06ad5b817858471fecf7b7f90c097b
Ronak Malik <ronakmalik4242@gmail.com>
2020-09-02 10:58:36 -0500

    Add debug mode to qemu script, and set starting memory to 4GB

--- qemu.sh
+++ qemu.sh
@@ -3,9 +3,17 @@ set -e
 ./scripts/clean.sh
 . ./scripts/iso.sh
 
+if [ "$1" = "debug" ]; then
+	qemu-system-"$(./scripts/target-triplet-to-arch.sh "$HOST")" -s -S \
+		-cdrom dentos.iso \
+		-serial stdio \
+		-m size=4096
+	exit
+fi
 qemu-system-"$(./scripts/target-triplet-to-arch.sh "$HOST")" \
   -cdrom dentos.iso \
-  -serial stdio
+  -serial stdio \
+  -m size=4096
 
 if [ "$1" = "clean" ]; then
   ./scripts/clean.sh
