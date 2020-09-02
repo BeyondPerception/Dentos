9484618d1b2c3d24a8fe519fc7198e98857a4390
Ronak Malik <ronakmalik4242@gmail.com>
2020-09-02 11:00:42 -0500

    Identity map from 0 to the end of the kernel to preserve multiboot structures

--- dentos/arch/i386/boot.s
+++ dentos/arch/i386/boot.s
@@ -3,12 +3,12 @@
 // Some magic numbers that need to be included
 // for grub to recognize the multiboot
 .set ALIGN, 1 << 0
-.set MEMINFO, 1 << 0
+.set MEMINFO, 1 << 1
 .set MAGIC, 0x1BADB002
 .set FLAGS, ALIGN | MEMINFO
 .set CHECKSUM, -(MAGIC + FLAGS)
 
-.section .multiboot
+.section .multiboot.data, "a"
 .align 4
 .long MAGIC
 .long FLAGS
@@ -29,18 +29,17 @@ boot_page_table1:
 
 .extern	kernel_main
 
-.section .text
+.section .multiboot.text, "ax"
 .global _start
 _start:
-    mov     edi, OFFSET boot_page_table1 - 0xC0000000
-    mov     esi, 0
+    // Identity map the 1st MB and then to the end of the kernel.
+    mov     edi, OFFSET boot_page_table1 - 0xC0000000 // Physical address of page_table0 (start of table)
+    mov     esi, 0 // Location of current page
 
-    // Map 1023 pages, the 1024th will be the VGA buffer
+    // Map 1023 pages (or until the end of the kernel), the 1024th will be the VGA buffer
     mov     ecx, 1023
 1:
 	// Only map the kernel
-    cmp     esi, OFFSET _kernel_start - 0xC0000000
-    jl      2f
     cmp     esi, OFFSET _kernel_end - 0xC0000000
     jge     3f
 
@@ -65,6 +64,7 @@ _start:
 	// would instead page fault if there was no identity mapping.
 
     // Map the page table to virtual addresses 0x00000000 and 0xC0000000
+    // Load the page tables into the page directory
     mov     DWORD PTR[boot_page_directory - 0xC0000000 + 0], OFFSET boot_page_table1 - 0xC0000000 + 0x3
 	mov		DWORD PTR[boot_page_directory - 0xC0000000 + 768 * 4], OFFSET boot_page_table1 - 0xC0000000 + 0x3
 
@@ -76,24 +76,26 @@ _start:
 	or		ecx, 0x80010000
 	mov		cr0, ecx
 
-	// Jump to high half
+	// Jump to higher half
 	lea		ecx, 4f
 	jmp		ecx
 
+.section .text
 4:
 	// Paging is now fully setup and enabled
 
-	// Unmap unnecessary identity mapping
-	mov		DWORD PTR[boot_page_directory], 0
-
-	// Reload crc3 to force a TLB flush so the changes to take effect.
+	// Reload cr3 to force a TLB flush so the changes to take effect.
 	mov		ecx, cr3
 	mov		cr3, ecx
 
-	// Start the stack pointer at the top of the stack to meet C specs
+    // Start the stack pointer at the top of the stack to meet C specs
 	mov		esp, OFFSET stack_top
 
-	// Call global constructors
+    // Push memory map information from GRUB onto stack for kernel
+    //add     ebx, 0xC0000000
+	push    ebx
+
+    // Call global constructors
 	call	_init
 
 	// Pass control to the kernel
@@ -104,7 +106,12 @@ _start:
 	cli // Disable CPU interrupts
 	hlt	// Halt the CPU
 	jmp     1b // Loop if the last two insturctions failed
-.size _start, . - _start
+//.size _start, . - _start
+
+.global unmap_kernel_lower
+unmap_kernel_lower:
+    mov     DWORD PTR[boot_page_directory], 0
+    ret
 
 gdtr:
     .short  0 // size of gdt
@@ -113,7 +120,6 @@ gdtr:
 .global gdt_set
 gdt_set:
     mov     eax, [esp + 4]
-    //add     eax, 0xC0000000
     mov     [gdtr + 2], eax
     mov     ax, [esp + 8]
     mov     [gdtr], ax
