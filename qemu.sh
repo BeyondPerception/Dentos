45e00aa713aee317e13284941cd79f69f87d8886
Ronak Malik <ronakmalik4242@gmail.com>
2020-11-12 10:34:17 -0600

    Add helpful message to debug option and only run qemu command once if debug option is selected

--- qemu.sh
+++ qemu.sh
@@ -4,17 +4,18 @@ set -e
 . ./scripts/iso.sh
 
 if [ "$1" = "debug" ]; then
+	echo "Starting gdb server on localhost:1234"
 	qemu-system-"$(./scripts/target-triplet-to-arch.sh "$HOST")" -s -S \
 		-cdrom dentos.iso \
 		-serial stdio \
 		-m size=4096
 	exit
+else
+	qemu-system-"$(./scripts/target-triplet-to-arch.sh "$HOST")" \
+  		-cdrom dentos.iso \
+  		-serial stdio \
+  		-m size=4096
 fi
-qemu-system-"$(./scripts/target-triplet-to-arch.sh "$HOST")" \
-  -cdrom dentos.iso \
-  -serial stdio \
-  -m size=4096
-
 if [ "$1" = "clean" ]; then
   ./scripts/clean.sh
 fi
