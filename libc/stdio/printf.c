904a387fb9e96e4c369b6d9674478fee9f0c4a8d
Ronak Malik <ronakmalik4242@gmail.com>
2021-01-11 16:32:57 -0600

    Switch to new vfprintf for intermediate functions

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -4,7 +4,7 @@
 int printf(const char* restrict format, ...) {
 	va_list args;
 	va_start(args, format);
-	int r = fprintf(STDOUT, format, args);
+	int r = vfprintf(STDOUT, format, args);
 	va_end(args);
 	return r;
 }
