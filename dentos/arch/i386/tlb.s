de9860e035eb9e27b8fd6a08aaf4ee74e6738de2
Ronak Malik <ronakmalik4242@gmail.com>
2021-01-19 13:20:46 -0600

    Add functions to flush the tlb and invalidate pages in the tlb

---
+++ dentos/arch/i386/tlb.s
@@ -0,0 +1,25 @@
+.intel_syntax noprefix
+
+// Invalidate a single page in the TLB
+.global invlpg
+invlpg:
+    push    ebp
+    mov     ebp, esp
+
+    mov     eax, dword ptr[ebp+4]
+    invlpg  [eax]
+
+    leave
+    ret
+
+// Do a complete reload of the TLB
+.global flushtlb
+flushtlb:
+    push    ebp
+    mov     ebp, esp
+
+    mov     eax, cr3
+    mov     cr3, eax
+
+    leave
+    ret
