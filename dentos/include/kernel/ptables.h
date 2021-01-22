f4cd7321afa2d2930a8a2148794b632140890aee
Ronak Malik <ronakmalik4242@gmail.com>
2021-01-22 10:09:44 -0600

    Add flags param to ptables header to match definition

--- dentos/include/kernel/ptables.h
+++ dentos/include/kernel/ptables.h
@@ -2,7 +2,7 @@
 #define DENTOS_PTABLES_H
 
 unsigned int ptables_init();
-int map_page(void* phys_addr, void* virt_addr);
+int map_page(void* phys_addr, void* virt_addr, int flags);
 int unmap_page(void* virt_addr);
 
 #endif //DENTOS_PTABLES_H
