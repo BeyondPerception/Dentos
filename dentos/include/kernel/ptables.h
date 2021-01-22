c90186402c421b052d6effd32e4d83df979d7c17
Ronak Malik <ronakmalik4242@gmail.com>
2021-01-22 10:15:28 -0600

    Add page flags to ptables header

--- dentos/include/kernel/ptables.h
+++ dentos/include/kernel/ptables.h
@@ -1,6 +1,12 @@
 #ifndef DENTOS_PTABLES_H
 #define DENTOS_PTABLES_H
 
+#define PTABLES_WRITABLE (1 << 1)
+#define PTABLES_USER (1 << 2)
+#define PTABLES_WRITETHROUGH_CACHE (1 << 3)
+#define PTABLES_DISABLE_CACHE (1 << 4)
+#define PTABLES_GLOBAL (1 << 8)
+
 unsigned int ptables_init();
 int map_page(void* phys_addr, void* virt_addr, int flags);
 int unmap_page(void* virt_addr);
