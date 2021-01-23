66f5d55ac161c5fd93ebcd7de8b98b7c099294cc
Ronak Malik <ronakmalik4242@gmail.com>
2021-01-22 19:44:43 -0600

    Remove fetch_pages and free_pages from header file

--- dentos/include/kernel/palloc.h
+++ dentos/include/kernel/palloc.h
@@ -2,9 +2,7 @@
 #define DENTOS_PALLOC_H
 
 void* next_page();
-void* fetch_pages(unsigned int n);
 void free_page();
-void free_pages();
 void palloc_init(multiboot_info_t* multiboot_info);
 
 #endif //DENTOS_PALLOC_H
